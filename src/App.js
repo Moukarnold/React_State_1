73a90b5d3722c940c5c796e3562825f767a32d38
Moukarnold <128094305+Moukarnold@users.noreply.github.com>
2023-04-24 16:40:51 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  //let isActive = false;
+  const [isActive, setState] = useState(false);
+
+  function handleClick() {
+    setState(!isActive);
+    // Check that the value changes correctly.
+    // console.log(isActive);
+  }
+
+  return (
+    <main>
+      <div className={`box ${isActive ? "box--active" : ""}`} />
+      <button onClick={handleClick}>Activate</button>
+    </main>
+  );
+}
